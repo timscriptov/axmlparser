797fd07891cd4884220eff3c2a03b0e1d8e3ddcd
Иван Тимашков <timscriptov@mail.ru>
2023-10-09 00:14:49 +0500

    SDK 17 Jitpack

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -19,7 +19,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.mcal"
             artifactId = "axmlparser"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["java"])
